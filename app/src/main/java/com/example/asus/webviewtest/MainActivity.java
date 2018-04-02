ff68979c1a09a4fed622207f35a2941377b2f93b
qingminghuanjing <919310171@qq.com>
2018-04-02 10:35:08 +0800

    第一提交

---
+++ app/src/main/java/com/example/asus/webviewtest/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.asus.webviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button btn_back;
+    private TextView txt_title;
+    private Button btn_top;
+    private Button btn_refresh;
+    private WebView wView;
+    private long exitTime = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+    }
+
+
+    private void bindViews() {
+        btn_back = (Button) findViewById(R.id.btn_back);
+        txt_title = (TextView) findViewById(R.id.txt_title);
+        btn_top = (Button) findViewById(R.id.btn_top);
+        btn_refresh = (Button) findViewById(R.id.btn_refresh);
+        wView = (WebView) findViewById(R.id.wView);
+
+        btn_back.setOnClickListener(this);
+        btn_refresh.setOnClickListener(this);
+        btn_top.setOnClickListener(this);
+
+        wView.loadUrl("http://www.baidu.com");
+        wView.setWebChromeClient(new WebChromeClient() {
+            //这里设置获取到的网站title
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                txt_title.setText(title);
+            }
+        });
+
+
+        wView.setWebViewClient(new WebViewClient() {
+            //在webview里打开新链接
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_back:
+                finish();          //关闭当前Activity
+                break;
+            case R.id.btn_refresh:
+                wView.reload();    //刷新当前页面
+                break;
+            case R.id.btn_top:
+                wView.setScrollY(0);   //滚动到顶部
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (wView.canGoBack()) {
+            wView.goBack();
+        } else {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(getApplicationContext(), "再按一次退出程序",
+                        Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+
+        }
+    }
+}
